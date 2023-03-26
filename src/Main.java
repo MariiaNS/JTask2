7d853eca71b5163ad9e74978c0c96c11270b86d9
Maria <smn0101@yandex.ru>
2023-03-26 20:46:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int score = 100;
+        int deposit = 1100;
+        int bonus;
+
+        if (deposit > 1000) {
+            bonus = deposit / 100;
+        } else {
+            bonus = 0;
+        }
+
+        int balance = score + deposit + bonus;
+        System.out.println("Здравствуйте! Ваш баланс: " + balance + " руб. Ваш бонус: " + bonus + " руб.");
+    }
+}
